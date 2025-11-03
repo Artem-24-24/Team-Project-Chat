161ff237fa31097f745be92003aae317258d1389
_KroZen_ <hannnttter@gmail.com>
2025-11-03 20:03:16 +0200

    Змінено заголовок клієнтської форми на українську мову

--- Team-Project-Chat/Form1.cs
+++ Team-Project-Chat/Form1.cs
@@ -5,7 +5,7 @@ namespace Team_Project_Chat
         public Form1()
         {
             InitializeComponent();
-            this.Text = "Team Project Chat Application";
+            this.Text = "Командний проект: Чат клієнта";
         }
     }
 }
