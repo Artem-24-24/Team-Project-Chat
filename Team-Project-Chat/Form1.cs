2a451e66a2f0b6944d090f4971513a441735d7eb
Артем <@DESKTOP-7RDB0JR>
2025-11-03 20:09:55 +0200

    Додав кнопку на форму клієнта

--- Team-Project-Chat/Form1.cs
+++ Team-Project-Chat/Form1.cs
@@ -6,5 +6,10 @@ namespace Team_Project_Chat
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("alallala");
+        }
     }
 }
